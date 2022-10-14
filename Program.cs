0dce5de50e328bce0191ac66c38c55ed2a44b753
Ivan M <slade91@mail.ru>
2022-10-14 15:57:10 +0300

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.WriteLine("Введите трехзначное число: ");
+string num = Convert.ToString(Console.ReadLine());
+
+if (num.Length >= 3)
+    Console.WriteLine(num[1]);
+
+else
+    Console.WriteLine("Число не трехзначное");
